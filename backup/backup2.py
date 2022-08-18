026def9664f018d2607952ff3a915f8be0f81490
SamGTex <samuel.haefs@tu-dortmund.de>
2022-08-18 13:55:18 +0200

    add file that will contains the tkinter windows

---
+++ backup/backup2.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+import os
+import time
+import datetime
+import argparse
+from tkinter import *
+
+from methods.bcolors import bcolors
+from methods.add import write
+from methods.clean import remove_duplicate
+from methods.paths import path
+
+
+
+window = Tk()
+
+window.title("Welcome to LikeGeeks app")
+
+window.mainloop()
